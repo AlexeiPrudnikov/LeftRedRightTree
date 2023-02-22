b7d92b1d7e2c67c91a7b08579519666167d83ae3
Alexei <alexei.prudnikov@gmail.com>
2023-02-22 09:21:20 +0600

    Modify main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         RedBlackTree tree = new RedBlackTree();
         int value = 0;
+        Random random = new Random();
         try {
-
             Scanner scanner = new Scanner(System.in);
             do {
                 System.out.print("Введите значение для добавления в левостороннее красно-черное дерево: ");
@@ -15,9 +16,8 @@ public class Main {
                 } else {
                     System.out.println("Значение не может быть добавлено.");
                 }
-            } while (1 == 1);
-        }
-        catch (Exception ex) {
+            } while (1==1);
+        } catch (Exception ex) {
             System.out.println("Работа программы завершена.");
         }
     }
